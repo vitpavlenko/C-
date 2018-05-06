bb9f27532cac59a3ecf0c7ced3e57631fbb1a047
vitpavlenko <madvet1@mail.ru>
2018-05-06 21:57:05 +0300

    first and second labs on C# KPI

---
+++ N1/laba2/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab1
+{
+    class Program
+    {
+        class Segment
+        {
+            public int point1;
+            public int point2;
+            public double cost;
+
+            public Segment(int point1, int point2, double cost)
+            {
+                this.point1 = point1;
+                this.point2 = point2;
+                this.cost = cost;
+            }
+        }
+
+        class Point
+        {
+            double x;
+            double y;
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public Point()
+            {
+                x = 0;
+                y = 0;
+            }
+
+            public double GetX()
+            {
+                return x;
+            }
+
+            public double GetY()
+            {
+                return y;
+            }
+
+            public void SetX(double x)
+            {
+                this.x = x;
+            }
+
+            public void SetY(double y)
+            {
+                this.y = y;
+            }
+        }
+
+        static private Point[] Points;
+        static private double[,] w;
+        static private int number;
+        static private int s4 = 0;
+        static private Segment[] tree;
+        static private Segment[] segments;
+        static private int m;
+        static private int m1;
+        static private bool[] visit;
+        static private HashSet<Segment> Allsegments;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter number of Points:");
+                String s = Console.ReadLine();
+                number = int.Parse(s);
+
+                Points = new Point[number];
+                w = new double[number, number];
+
+                for (int i = 0; i < number; i++)
+                    for (int j = 0; j < number; j++)
+                        w[i, j] = Double.PositiveInfinity;
+
+                Console.WriteLine("Enter your choise:\n1. Generate Points\n2. Enter Points");
+                char choise = Convert.ToChar(Console.ReadLine());
+                switch (choise)
+                {
+                    case '1':
+                        GeneratePoints();
+                        break;
+                    case '2':
+                        EnterPoints();
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input");
+                        break;
+                }
+
+                m1 = (number * (number - 1) / 2);
+
+                segments = new Segment[m1];
+                Allsegments = new HashSet<Segment>();
+                tree = new Segment[m1];
+
+                Intialize();
+                
+                visit = new bool[number];
+
+                PrimAlgorithm();
+
+                Output();
+            } catch {
+                Console.WriteLine("Incorrect input");
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadKey();
+            }
+        }
+
+        static double GetCost(Point point1, Point point2)
+        {
+            return Math.Sqrt(Math.Pow((point1.GetX() - point2.GetX()), 2) + Math.Pow((point1.GetY() - point2.GetY()), 2));
+        }
+
+        static void Output()
+        {
+            for (int i = 0; i < s4; i++)
+            {
+                Console.WriteLine("\nSegment №" + (i + 1) + ":");
+                Console.WriteLine("Point №" + tree[i].point1 + ":\nx: " + Points[tree[i].point1 - 1].GetX() + "  y: " + Points[tree[i].point1 - 1].GetY());
+                Console.WriteLine("Point №" + tree[i].point2 + ":\nx: " + Points[tree[i].point2 - 1].GetX() + "  y: " + Points[tree[i].point2 - 1].GetY());
+                Console.WriteLine("Cost:" + tree[i].cost);
+            }
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+
+        static void GeneratePoints()
+        {
+            Random random = new Random();
+            for (int i = 0; i < number; i++)
+            {
+                double x = random.Next(-100, 100);
+                double y = random.Next(-100, 100);
+
+                Points[i] = new Point(x, y);
+            }
+        }
+
+        static void EnterPoints()
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Point №" + (i + 1) + "\nEnter x: ");
+                String s1 = Console.ReadLine();
+                double x = double.Parse(s1);
+
+                Console.WriteLine("Enter y: ");
+                String s2 = Console.ReadLine();
+                double y = double.Parse(s2);
+
+                Points[i] = new Point(x, y);
+            }
+        }
+
+        static void Intialize()
+        {
+            int index = 0;
+            for (int i = 0; i < number; i++)
+                for (int j = 0; j < number; j++)
+                {
+                    if (i < j)
+                    {
+                        segments[index++] = new Segment(i, j, GetCost(Points[i], Points[j]));
+                        w[i, j] = GetCost(Points[i], Points[j]);
+                        w[j, i] = w[i, j];
+                    }
+                }
+        }
+
+        static void GenerateSegments()
+        {
+            Random random = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    int x = random.Next(0, number);
+                    int y = random.Next(0, number);
+
+                    if (Allsegments.Add(new Segment(x, y, 0)) && x != y)
+                    {
+                        segments[i] = new Segment(x - 1, y - 1,
+                            
+                            0);
+                        w[x, y] = 0;
+                        w[y, x] = 0;
+                        flag = false;
+                    }
+                }
+            }
+        }
+
+        static void PrimAlgorithm()
+        {
+            for (int i = 0; i < number; i++)
+            {
+                visit[i] = false;
+            }
+
+            bool e = true;
+            double segment = Double.PositiveInfinity;
+            int v1 = -1;
+            for (int i = 0; i < m1; i++)
+            {
+                if (segment > segments[i].cost)
+                {
+                    segment = segments[i].cost;
+                    v1 = segments[i].point1;
+                }
+            }
+            visit[v1] = true;
+            while (e)
+            {
+                e = false;
+                bool e1 = false;
+                if (!e1)
+                {
+                    int point1 = -1;
+                    int point2 = -1;
+                    double segment1 = Double.PositiveInfinity;
+                    for (int i = 0; i < number; i++)
+                    {
+                        for (int j = 0; j < number; j++)
+                        {
+                            if ((visit[i] == true) && (visit[j] == false) && (w[i, j] < segment1))
+                            {
+                                point1 = i;
+                                point2 = j;
+                                segment1 = w[i, j];
+                            }
+                        }
+
+                    }
+                    if ((point1 != -1) && (point2 != -1))
+                    {
+                        e1 = true;
+                        visit[point2] = true;
+                        tree[s4++] = new Segment(point1 + 1, point2 + 1, segment1);
+                    }
+                }
+                for (int i = 0; i < number; i++)
+                    if (visit[i] == false)
+                        e = true;
+            }
+        }
+    }
+}
---
+++ N2/claba2/Program.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] p = new string[1];
+            p[0] = "duity";
+
+            Worker[] w = new Worker[1];
+            w[0] = new Coder("second", "secondBoss", 288, p, null);
+
+            Worker[] w1 = new Worker[2];
+            w1[0] = new Coder("second", "secondBoss", 123, p, null);
+            w1[1] = new Coder("seventh", "secondBoss", 321, p, null);
+
+            Group MyGroup = new Group();
+            MyGroup.Changing += ChangingListener;
+            MyGroup.Add(new Manager("first", "firstBoss", 1488, p, w));
+            MyGroup.Add(new Coder("third", "firstBoss", 328, p, w1));
+            MyGroup.Insert(0, new Developer("fivs", "firstBoss", 69, p, w1) + new Developer("six", "sixBoss", 47, p, w));
+            MyGroup.Delete(1);
+            Console.WriteLine(MyGroup);
+
+            try
+            {
+                MyGroup.Get(100);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("IndexOutOfRangeException");
+            }
+
+            Console.WriteLine(MyGroup.Get(0));
+            Console.WriteLine((Developer)MyGroup.Get(0) == new Developer("fivs", "firstBoss", 116, p, w1));
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+
+        public static void ChangingListener(ChangingType change)
+        {
+            switch (change)
+            {
+                case ChangingType.Add:
+                    Console.WriteLine("Added Element");
+                    break;
+                case ChangingType.Delete:
+                    Console.WriteLine("Deleted Element");
+                    break;
+                case ChangingType.Set:
+                    Console.WriteLine("Setted Element");
+                    break;
+                case ChangingType.Insert:
+                    Console.WriteLine("Inserted Element");
+                    break;
+            }
+        }
+    }
+
+
+    public abstract class Worker
+    {
+        public string surname;
+        public string bossSurname;
+        public string[] duties;
+        public Worker []subordinates;
+        public int salary;
+        public abstract string ListDuties();
+        public abstract string ListOfSubordinates();
+
+        public abstract override string ToString();
+        public abstract override bool Equals(object obj);
+        public abstract override int GetHashCode();
+    }
+
+    public class Manager : Worker
+    {
+        public Manager(string surname, string bossSurname, int salary, string[] duties, Worker[] subordinates)
+        {
+            base.surname = surname;
+            base.bossSurname = bossSurname;
+            base.subordinates = subordinates;
+            base.duties = duties;
+            base.salary = salary;
+        }
+
+        public Manager() { }
+
+        public override string ListDuties()
+        {
+            var res = "";
+            foreach (var r in base.duties)
+                res += r + " ";
+            return res;
+        }
+
+        public override string ListOfSubordinates()
+        {
+            var res = "";
+            foreach (var r in base.subordinates)
+            {
+                res += " name: " + r.surname + " duties: " + r.ListDuties();
+            }
+
+            return res;
+        }
+
+        public static Manager operator +(Manager x, Manager y)
+        {
+            Manager tmp = new Manager();
+            tmp.surname = x.surname;
+            tmp.bossSurname = x.bossSurname;
+            tmp.salary = x.salary + y.salary;
+
+            string[] tm = new string[x.duties.Length + y.duties.Length];
+            Array.Copy(x.duties, tm, x.duties.Length);
+            Array.Copy(y.duties, tm, y.duties.Length);
+            tmp.duties = tm;
+
+            Worker[] tm1 = new Worker[x.subordinates.Length + y.subordinates.Length];
+            Array.Copy(x.subordinates, tm1, x.subordinates.Length);
+            Array.Copy(y.subordinates, tm1, y.subordinates.Length);
+            tmp.subordinates = tm1;
+
+            return tmp;
+        }
+
+        public override bool Equals(object x)
+        {
+            Manager obj = (Manager)x;
+            if (this.surname == obj.surname && this.salary == obj.salary && this.bossSurname == obj.bossSurname) return true;
+            else return false;
+        }
+
+        public static bool operator ==(Manager x, Manager y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(Manager x, Manager y)
+        {
+            return !Equals(x, y);
+        }
+
+        public override int GetHashCode()
+        {
+            return surname.GetHashCode() + 13 * bossSurname.GetHashCode() + 169 * salary.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "Manager: (" + surname.ToString() + "," +
+                   bossSurname.ToString() + "," + salary.ToString() + ")";
+        }
+
+    }
+
+    public class Developer : Worker
+    {
+
+        public Developer(string surname, string bossSurname, int salary, string[] duties, Worker[] subordinates)
+        {
+            base.surname = surname;
+            base.bossSurname = bossSurname;
+            base.subordinates = subordinates;
+            base.duties = duties;
+            base.salary = salary;
+        }
+
+        public Developer() { }
+
+
+        public override string ListDuties()
+        {
+            var res = "";
+            foreach (var r in base.duties)
+                res += r + " ";
+            return res;
+        }
+
+        public override string ListOfSubordinates()
+        {
+            var res = "";
+            foreach (var r in base.subordinates)
+            {
+                res += " name: " + r.surname + " duties: " + r.ListDuties();
+            }
+
+            return res;
+        }
+
+        public static Developer operator +(Developer x, Developer y)
+        {
+            Developer tmp = new Developer();
+            tmp.surname = x.surname;
+            tmp.bossSurname = x.bossSurname;
+            tmp.salary = x.salary + y.salary;
+
+            string[] tm = new string[x.duties.Length + y.duties.Length];
+            Array.Copy(x.duties, tm, x.duties.Length);
+            Array.Copy(y.duties, tm, y.duties.Length);
+            tmp.duties = tm;
+
+            Worker[] tm1 = new Worker[x.subordinates.Length + y.subordinates.Length];
+            Array.Copy(x.subordinates, tm1, x.subordinates.Length);
+            Array.Copy(y.subordinates, tm1, y.subordinates.Length);
+            tmp.subordinates = tm1;
+
+            return tmp;
+        }
+
+        public override bool Equals(object x)
+        {
+            Developer obj = (Developer)x;
+            if (this.surname == obj.surname && this.salary == obj.salary && this.bossSurname == obj.bossSurname) return true;
+            else return false;
+        }
+
+        public static bool operator ==(Developer x, Developer y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(Developer x, Developer y)
+        {
+            return !Equals(x, y);
+        }
+
+        public override int GetHashCode()
+        {
+            return surname.GetHashCode() + 13 * bossSurname.GetHashCode() + 169 * salary.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "Developer: (" + surname.ToString() + "," +
+                   bossSurname.ToString() + "," + salary.ToString() + ")";
+        }
+    }
+
+    public class Coder : Worker
+    {
+        public Coder(string surname, string bossSurname, int salary, string[] duties, Worker[] subordinates)
+        {
+            base.surname = surname;
+            base.bossSurname = bossSurname;
+            base.subordinates = subordinates;
+            base.duties = duties;
+            base.salary = salary;
+        }
+
+        public Coder() { }
+
+
+        public override string ListDuties()
+        {
+            var res = "";
+            foreach (var r in base.duties)
+                res += r + " ";
+            return res;
+        }
+
+        public override string ListOfSubordinates()
+        {
+            var res = "";
+            foreach (var r in base.subordinates)
+            {
+                res += " name: " + r.surname + " duties: " + r.ListDuties();
+            }
+
+            return res;
+        }
+
+        public static Coder operator +(Coder x, Coder y)
+        {
+            Coder tmp = new Coder();
+            tmp.surname = x.surname;
+            tmp.bossSurname = x.bossSurname;
+            tmp.salary = x.salary + y.salary;
+
+            string[] tm = new string[x.duties.Length + y.duties.Length];
+            Array.Copy(x.duties, tm, x.duties.Length);
+            Array.Copy(y.duties, tm, y.duties.Length);
+            tmp.duties = tm;
+
+            Worker[] tm1 = new Worker[x.subordinates.Length + y.subordinates.Length];
+            Array.Copy(x.subordinates, tm1, x.subordinates.Length);
+            Array.Copy(y.subordinates, tm1, y.subordinates.Length);
+            tmp.subordinates = tm1;
+
+            return tmp;
+        }
+
+        public override bool Equals(object x)
+        {
+            Coder obj = (Coder)x;
+            if (this.surname == obj.surname && this.salary == obj.salary && this.bossSurname == obj.bossSurname) return true;
+            else return false;
+        }
+
+        public static bool operator ==(Coder x, Coder y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(Coder x, Coder y)
+        {
+            return !Equals(x, y);
+        }
+
+        public override int GetHashCode()
+        {
+            return surname.GetHashCode() + 13 * bossSurname.GetHashCode() + 169 * salary.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "Coder: (" + surname.ToString() + "," +
+                   bossSurname.ToString() + "," + salary.ToString() + ")";
+        }
+    }
+
+    public enum ChangingType { Add, Insert, Delete, Set }
+    public delegate void ChangeHandler(ChangingType change);
+
+    public class Group
+    {
+        public List<Worker> Elements
+        {
+            get;
+        }
+
+        public event ChangeHandler Changing;
+
+        public Group()
+        {
+            Elements = new List<Worker>();
+        }
+
+        public void Add(Worker element)
+        {
+            Elements.Add(element);
+            if (Changing != null) Changing(ChangingType.Add);
+        }
+
+        public Worker Get(int index)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            return Elements[index];
+        }
+
+        public void Set(int index, Worker element)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            Elements[index] = element;
+            if (Changing != null) Changing(ChangingType.Set);
+        }
+
+        public void Insert(int index, Worker element)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            Elements.Insert(index, element);
+            if (Changing != null) Changing(ChangingType.Insert);
+        }
+
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            Elements.RemoveAt(index);
+            if (Changing != null) Changing(ChangingType.Delete);
+        }
+
+        public Worker getBySurname(string surname)
+        {
+            foreach (Worker k in Elements)
+            {
+                if (k.surname == surname)
+                {
+                    return k;
+                }
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "\nMy Group:\n" + string.Join("\n", Elements) + "\n";
+        }
+    }
+
+    public class MyExeption : Exception
+    {
+        MyExeption(String str) : base("MyExeption: " + str) {}
+    }
+}
